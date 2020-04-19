92ae80ef35aa3b1f340dc3e21b39b0f790d7cc1e
isaac calvo ochoa <isaaccalvoochoa@gmail.com>
2020-04-19 00:22:17 -0700

    Funcionando hasta inicio video06

---
+++ Taxi.Web/Data/Entities/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Taxi.Web.Data.Entities
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<TaxiEntity> Taxis { get; set; }
+    }
+}
